f6d55db4d964f88a91c5404a701e944d65f4ac0d
Anurag Zalke <81683716+Anurag-Zalke@users.noreply.github.com>
2024-01-23 11:14:20 +0530

    Changed Access Token

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_URL = "https://api.github.com/users/";
-const ACCESS_TOKEN = "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";
+const ACCESS_TOKEN = "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";
 
 const main = document.getElementById("main");
 const searchBox = document.getElementById("search");
